d4aaf9369264eb932b2fe0c702ce77ca5aae6ded
thisisaaronland <aaron@mapzen.com>
2015-09-29 23:20:06 +0000

    update dependency links; update README; bump to 0.04

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ desc = open("README.md").read(),
 setup(
     name='mapzen.whosonfirst.validator',
     namespace_packages=['mapzen', 'mapzen.whosonfirst', 'mapzen.whosonfirst.validator'],
-    version='0.03',
+    version='0.04',
     description='Tools for validating Who\'s On First documents',
     author='Mapzen',
-    url='https://github.com/mapzen/py-mapzen-whosonfirst-validator',
+    url='https://github.com/whosonfirst/py-mapzen-whosonfirst-validator',
     install_requires=[
         'mapzen.whosonfirst.utils',
         'mapzen.whosonfirst.export',
@@ -19,10 +19,13 @@ setup(
         'geojson',
         ],
     dependency_links=[
+        'https://github.com/whosonfirst/py-mapzen-whosonfirst-utils/tarball/master#egg=mapzen.whosonfirst.utils-0.05',
+        'https://github.com/whosonfirst/py-mapzen-whosonfirst-export/tarball/master#egg=mapzen.whosonfirst.export-0.59',
+        'https://github.com/whosonfirst/py-mapzen-whosonfirst-placetypes/tarball/master#egg=mapzen.whosonfirst.placetypes-0.04',
         ],
     packages=packages,
     scripts=[
         'scripts/wof-validator',
         ],
-    download_url='https://github.com/mapzen/py-mapzen-whosonfirst-validator/releases/tag/v0.03',
+    download_url='https://github.com/mapzen/py-mapzen-whosonfirst-validator/releases/tag/v0.04',
     license='BSD')
